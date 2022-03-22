f99fb1cef8975b35eb6f8e57976e6e502acf39fc
bektur14 <81956059+bektur14@users.noreply.github.com>
2022-03-22 12:10:53 +0600

    to-do

---
+++ src/components/TodoForm.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      {props.edit ? (
+        <>
+          <input
+            placeholder="Задать новую задачу"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            ref={inputRef}
+            className="todo-input edit"
+          />
+          <button onClick={handleSubmit} className="todo-button edit">
+            Обновить
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder="Что вы хотите сделать сегодня"
+            value={input}
+            onChange={handleChange}
+            name="text"
+            className="todo-input"
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className="todo-button">
+            Добавить в список
+          </button>
+        </>
+      )}
+    </form>
+  );
+}
+
+export default TodoForm;
